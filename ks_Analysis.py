9674227ce8cf9e035af651ec0e109b4fc0ea035e
CU-tmoney <terren.crider@ucdenver.edu>
2020-03-29 22:17:03 -0600

    very early cursory analysis of variables

--- ks_Analysis.py
+++ ks_Analysis.py
@@ -2,10 +2,26 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 # create ks dataframe
 ks = pd.DataFrame(pd.read_csv("ks.csv"))
+print(list(ks.columns))
+
+# create boolean column for failed/successful
+ks['state_bool'] = ks['state'].replace({'failed': False, 'successful': True}).astype(bool)
 
 # correlation of columns
-ks_Cor = ks.corr(method='pearson')
+ks_Cor = ks.corr()
+
+# try to find variables with high correlation to state_bool
+ks_CorTarget = abs(ks_Cor['state_bool'])
+rel_Feat = ks_CorTarget[ks_CorTarget>0]
+print(rel_Feat)
+'''
+No good variables are found.
+Using a boolean (non-continuous) Y means we'll probably need to look at other methods of variable selection
+and model building.
+'''

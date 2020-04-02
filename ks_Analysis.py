0a78869c5958cd92dd30fd46bbc6213ca2131580
CU-tmoney <terren.crider@ucdenver.edu>
2020-04-01 19:11:53 -0600

    Cleaned up old code

--- ks_Analysis.py
+++ ks_Analysis.py
@@ -222,25 +222,3 @@ reg = linear_model.LogisticRegression(solver='lbfgs')
 
 model = reg.fit(xTrain,yTrain)
 
-
-"""
-# create boolean column for failed/successful
-ks['state_bool'] = ks['state'].replace({'failed': False, 'successful': True}).astype(bool)
-
-# correlation of columns
-ks_Cor = ks.corr()
-
-# try to find variables with high correlation to state_bool
-ks_CorTarget = abs(ks_Cor['state_bool'])
-rel_Feat = ks_CorTarget[ks_CorTarget > 0]  # change value to be more selective
-print(rel_Feat)
-'''
-No good variables are found.
-Using a boolean (non-continuous) Y means we'll probably need to look at other methods of variable selection
-and model building.
-'''
-
-'''
-CREATE DUMMY VARIABLES (OR RECODE AS INT) FOR OTHER CATEGORICAL COLUMNS TO ALLOW CORRELATION CALCULATION.
-'''
-"""
